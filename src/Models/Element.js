43d26706817873b45fe369a63393601b1a86a0d4
Ivan Rodriguez Carrion <irodriguez@vbote.com>
2020-06-02 16:24:57 +0200

    first commit with all package dependencies and Element model.

---
+++ src/Models/Element.js
@@ -0,0 +1,55 @@
+class Element {
+
+    //al properties.
+    constructor(atomicMass, atomicNumber, atomicRadius, boilingPoint, bondingType, cpkHexColor, density, electronAffinity, electronegativity, electronicConfiguration,
+        groupBlock, ionRadius, ionizationEnergy, meltingPoint, name, oxidationStates, standardState, symbol, vanDelWaalsRadius, yearDiscovered) {
+
+        atomicMass = this.atomicMass;
+        atomicNumber = this.atomicNumber;
+        atomicRadius = this.atomicRadius;
+        boilingPoint = this.boilingPoint;
+        bondingType = this.bondingType;
+        cpkHexColor = this.cpkHexColor;
+        density = this.density;
+        electronAffinity = this.electronAffinity;
+        electronegativity = this.electronegativity;
+        electronicConfiguration = this.electronicConfiguration;
+        groupBlock = this.groupBlock;
+        ionRadius = this.ionRadius;
+        ionizationEnergy = this.ionizationEnergy;
+        meltingPoint = this.meltingPoint;
+        name = this.name;
+        oxidationStates = this.oxidationStates;
+        standardState = this.standardState;
+        symbol = this.symbol;
+        vanDelWaalsRadius = this.vanDelWaalsRadius;
+        yearDiscovered = this.yearDiscovered;
+
+
+    }
+
+    get atomicMass() { return this.atomicMass; }
+    get atomicNumber() { return this.atomicNumber; }
+    get atomicRadius() { return this.atomicRadius; }
+    get boilingPoint() { return this.boilingPoint; }
+    get bondingType() { return this.bondingType; }
+    get cpkHexColor() { return this.cpkHexColor; }
+    get density() { return this.density; }
+    get electronAffinity() { return this.electronAffinity; }
+    get electronegativity() { return this.electronegativity; }
+    get electronicConfiguration() { return this.electronicConfiguration; }
+    get groupBlock() { return this.groupBlock; }
+    get ionRadius() { return this.ionRadius; }
+    get ionizationEnergy() { return this.ionizationEnergy; }
+    get meltingPoint() { return this.meltingPoint; }
+    get name() { return this.name; }
+    get oxidationStates() { return this.oxidationStates; }
+    get standardState() { return this.standardState; }
+    get symbol() { return this.symbol; }
+    get vanDelWaalsRadius() { return this.vanDelWaalsRadius; }
+    get yearDiscovered() { return this.yearDiscovered; }
+
+
+
+
+}
